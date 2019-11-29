7fe46a5e5456ac6f561ccf1ce3fcc2b24a988473
zhangcz828 <zhangcz828@126.com>
2019-11-29 13:27:28 +0800

    init

---
+++ pkg/apis/samplecrd/v1/types.go
@@ -0,0 +1,45 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +genclient:noStatus
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Network describes a Network resource
+type Foo struct {
+	// TypeMeta is the metadata for the resource, like kind and apiversion
+	metav1.TypeMeta `json:",inline"`
+	// ObjectMeta contains the metadata for the particular object, including
+	// things like...
+	//  - name
+	//  - namespace
+	//  - self link
+	//  - labels
+	//  - ... etc ...
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Spec is the custom resource spec
+	Spec FooSpec `json:"spec"`
+}
+
+// NetworkSpec is the spec for a Network resource
+type FooSpec struct {
+	// Cidr and Gateway are example custom spec fields
+	//
+	// this is where you would put your custom resource data
+	Cidr    string `json:"cidr"`
+	Gateway string `json:"gateway"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NetworkList is a list of Network resources
+type FooList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []Foo `json:"items"`
+}
